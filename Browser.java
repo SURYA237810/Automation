16eee64694e235d86eda8bdcd80e107b1cd972a7
SURYA237810 <ssurya80611@gmail.com>
2025-07-15 21:50:26 +0530

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,20 @@
+package week1;
+
+public class Browser {
+	public void lanchBrowser() {
+		System.out.println("browser lanched successfully");
+
+	}
+
+	public void loadUrl() {
+		System.out.println("url loaded successfully");
+
+	}
+
+	public static void main(String[] arg) {
+		Browser obj = new Browser();
+		obj.lanchBrowser();
+		obj.loadUrl();
+
+	}
+}
---
+++ firefox.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion=1002f;
+String browserName="firefox";
+boolean isVisible=true;
+int releaseYear=1998;
+char browserLogo='f';
+System.out.println(browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo);
+	}
+
+}
